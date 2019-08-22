1eb180b8b82d10ee573c8043a4e15b866847fcef
dev2054 <53892688+dev2054@users.noreply.github.com>
2019-08-22 12:39:05 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+def fib(n):
+    b=0
+    a=1
+    print("0")
+    print('1')
+    while n!=0:
+        c=a
+        a=a+b
+        b=c
+        n=n-1
+        print(a)
+n=int(input('enter the number of fibonacci sequence u want'))
+if n==0:
+    print("0")
+elif n==1:
+    print("1")
+elif n==2:
+    print("1")
+else:
+    fib(n)
+
+            
+
